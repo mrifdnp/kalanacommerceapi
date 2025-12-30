d68f2929a899b3cb1c965da4912376d7bcda60f2
Rifqi Dani Putranto <rifqidani23@gmail.com>
2025-12-30 10:15:37 +0700

    fix cart

--- src/validations/cart.validation.ts
+++ src/validations/cart.validation.ts
@@ -1,10 +1,7 @@
 import Joi from 'joi';
 
 const addCartItemSchema = Joi.object({
-    productId: Joi.string().uuid().required().messages({
-        'string.guid': 'Format ID Produk tidak valid.',
-        'any.required': 'ID Produk wajib diisi.'
-    }),
+   
     quantity: Joi.number().integer().min(1).required().messages({
         'number.min': 'Jumlah item minimal 1.',
         'any.required': 'Kuantitas wajib diisi.'
