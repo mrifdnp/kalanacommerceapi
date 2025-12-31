47810345c591126a045c13428f6fe1f4d337c335
Rifqi Dani Putranto <rifqidani23@gmail.com>
2025-12-31 16:10:11 +0700

    fix:profile endpoint

--- src/routes/profile.route.ts
+++ src/routes/profile.route.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 // Asumsi getMe ada di controllers/user.controller.ts
-import { getMe } from '../controllers/user.controller.js'; 
+import { getMe, updateProfile } from '../controllers/user.controller.js'; 
 import { authenticateToken } from '../middlewares/auth.middleware.js';
 
 const profileRouter = Router();
@@ -8,5 +8,6 @@ const profileRouter = Router();
 // Route GET / (yang akan dipetakan ke /api/me)
 // Middleware: Hanya perlu authenticateToken
 profileRouter.get('/', authenticateToken, getMe); 
+profileRouter.put('/update', authenticateToken,updateProfile)
 
 export default profileRouter;
